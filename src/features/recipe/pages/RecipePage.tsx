b6c124e078ac661c0e35d117b43d019e9430b91c
Matheus Leal <matheusaquino@id.uff.br>
2021-01-11 22:14:12 -0300

    receipe page

---
+++ src/features/recipe/pages/RecipePage.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import DefaultStyle from '../../../style/default.style';
+
+export default ({
+	match
+}) => {
+	const [receipe, setReceipe] = useState(
+		{
+			id: match.params.id,
+			name: match.params.id,
+			steps: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur quam dolor, non finibus orci volutpat quis. Vestibulum sagittis, neque nec mollis varius, ex tellus commodo justo,vitae eleifend mi diam et ipsum. Vivamus ac ornare nunc. Nam finibus, ante at
+				sollicitudin efficitur, tellus eros vehicula sem, vel scelerisque augue augue a felis. Morbi enim urna, porta vel pulvinar vitae, tempus a elit. In vel fermentum mi. Quisque a dolor erat. Nunc ut vehicula ligula. Nunc fringilla fringilla lacus, ac euismod ante malesuada in. Integer mauris sem, ultricies eu luctus vitae, tincidunt vitae nunc.`
+		},
+	);
+
+	return (
+		<View style={styles.container}>
+			<Text>Nome: {receipe.name}</Text>
+			<Text>{receipe.steps}</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: DefaultStyle.container,
+});
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
 import RecipesPage from './features/recipe/pages/RecipesPage';
+import RecipePage from './features/recipe/pages/RecipePage';
 
 export default [
 	{
 		path: '/',
 		exact: true,
 		main: RecipesPage
+	},
+	{
+		path: '/receipe/:id',
+		main: RecipePage
 	}
 ]
